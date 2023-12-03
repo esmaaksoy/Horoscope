eeb70ee0a747cfe96308d3aa776fbd3e4fa23c95
esma <esma.ea.aksoy@gmail.com>
2023-12-03 18:06:27 +0300

    style

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,14 +1,13 @@
 import "./Main.scss"
 const Card = ({title,date,desc,image}) => {
   return (
-    <div >
-        <div className="card">
+    <div className="card">
+        <div className="image" >
             <img src={image} alt="" />
             <p className="title">{title}</p>
             <span>{date}</span>
-            <p >{desc}</p>
         </div>
-
+        <p className="desc">{desc}</p>
     </div>
   );
 };
